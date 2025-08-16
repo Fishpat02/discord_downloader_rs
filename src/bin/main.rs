3939c9e945ca5cd329f28a7653dff6eec78f77fc
Fishpat02 <55009589+Fishpat02@users.noreply.github.com>
2025-08-16 17:08:15 -0400

    Initial commit

---
+++ src/bin/main.rs
@@ -0,0 +1,29 @@
+use discord_scraper::*;
+use poise::serenity_prelude as serenity;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    dotenvy::dotenv()?;
+
+    let token = dotenvy::var("TOKEN")?;
+    let intents = serenity::GatewayIntents::non_privileged();
+
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![age()],
+            ..Default::default()
+        })
+        .setup(|ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {})
+            })
+        })
+        .build();
+
+    let client = serenity::ClientBuilder::new(token, intents)
+        .framework(framework)
+        .await;
+
+    Ok(client.unwrap().start().await?)
+}
