2de9aa126d7474c39c0d7cf6ca17d888160b09b3
Fishpat02 <55009589+Fishpat02@users.noreply.github.com>
2025-08-25 11:20:49 -0400

    Added bin target to remove all globally registered commands

---
+++ src/bin/unregister.rs
@@ -0,0 +1,31 @@
+use discord_scraper::*;
+use poise::serenity_prelude as serenity;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    dotenvy::dotenv()?;
+
+    let token = dotenvy::var("TOKEN")?;
+    let intents = serenity::GatewayIntents::all();
+
+    let framework = poise::Framework::builder()
+        .setup(|_ctx, _ready, _framework: &poise::Framework<Data, Error>| {
+            Box::pin(async move { Ok(Data {}) })
+        })
+        .options(poise::FrameworkOptions::default())
+        .build();
+
+    let client = serenity::ClientBuilder::new(token, intents)
+        .application_id(serenity::ApplicationId::new(
+            dotenvy::var("CLIENT")?.parse::<u64>()?,
+        ))
+        .framework(framework)
+        .await?;
+
+    let commands = (&client).http.get_global_commands().await?;
+    for cmd in commands {
+        (&client).http.delete_global_command(cmd.id).await?;
+    }
+
+    Ok(())
+}
