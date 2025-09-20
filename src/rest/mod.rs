06612e43d103189f75f2dcd0857c0ec5ca652e00
Fishpat02 <55009589+Fishpat02@users.noreply.github.com>
2025-09-20 16:20:05 -0400

    Moved rest module functions into MessageLogger struct

--- src/rest/mod.rs
+++ src/rest/mod.rs
@@ -8,62 +8,73 @@ use crate::Error;
 use serenity::{Channel, ChannelId, Http, Message, MessagePagination};
 use std::sync::Arc;
 
-#[allow(dead_code)]
-pub async fn get_channels(http: Arc<Http>, ids: Vec<ChannelId>) -> Result<Vec<Channel>, Error> {
-    let mut channels = vec![];
-
-    for id in ids {
-        channels.push(http.get_channel(id).await?);
-    }
-
-    Ok(channels)
+pub struct MessageLogger {
+    http: Arc<Http>,
 }
 
-#[allow(dead_code)]
-pub async fn get_messages_in_channel(
-    http: Arc<Http>,
-    channel: Channel,
-) -> Result<Vec<Message>, Error> {
-    let mut messages = vec![];
-    let mut last_message: Option<&Message> = None;
-
-    loop {
-        if let Some(_) = last_message {
-            let target = MessagePagination::Before(last_message.unwrap().id);
-            let mut batch = http.get_messages(channel.id(), Some(target), Some(100)).await?;
-            let batch_len = batch.len();
-
-            if batch_len > 0 {
-                messages.append(&mut batch);
-                last_message = messages.last();
-
-                println!(
-                    "Retrieved {} messages in channel {} from {}",
-                    batch_len,
-                    channel,
-                    last_message.unwrap().timestamp
-                );
-            } else {
-                break;
-            }
-        } else {
-            let mut batch = http.get_messages(channel.id(), None, Some(50)).await?;
-
-            if batch.len() > 0 {
-                messages.append(&mut batch);
-                last_message = messages.last();
-
-                println!(
-                    "Retrieved {} messages in channel {} from {}",
-                    messages.len(),
-                    channel,
-                    last_message.unwrap().timestamp
-                );
-            } else {
-                break;
-            }
-        }
+impl MessageLogger {
+    #[allow(dead_code)]
+    pub fn new(http: Arc<Http>) -> Self {
+        MessageLogger { http: http }
     }
 
-    Ok(messages)
+    #[allow(dead_code)]
+    pub async fn get_channels(&self, ids: Vec<ChannelId>) -> Result<Vec<Channel>, Error> {
+        let mut channels = vec![];
+
+        for id in ids {
+            channels.push(self.http.get_channel(id).await?);
+        }
+
+        Ok(channels)
+    }
+
+    #[allow(dead_code)]
+    pub async fn get_messages_in_channel(&self, channel: Channel) -> Result<Vec<Message>, Error> {
+        let mut messages = vec![];
+        let mut last_message: Option<&Message> = None;
+
+        loop {
+            if let Some(_) = last_message {
+                let target = MessagePagination::Before(last_message.unwrap().id);
+                let mut batch = self
+                    .http
+                    .get_messages(channel.id(), Some(target), Some(100))
+                    .await?;
+                let batch_len = batch.len();
+
+                if batch_len > 0 {
+                    messages.append(&mut batch);
+                    last_message = messages.last();
+
+                    println!(
+                        "Retrieved {} messages in channel {} from {}",
+                        batch_len,
+                        channel,
+                        last_message.unwrap().timestamp
+                    );
+                } else {
+                    break;
+                }
+            } else {
+                let mut batch = self.http.get_messages(channel.id(), None, Some(50)).await?;
+
+                if batch.len() > 0 {
+                    messages.append(&mut batch);
+                    last_message = messages.last();
+
+                    println!(
+                        "Retrieved {} messages in channel {} from {}",
+                        messages.len(),
+                        channel,
+                        last_message.unwrap().timestamp
+                    );
+                } else {
+                    break;
+                }
+            }
+        }
+
+        Ok(messages)
+    }
 }
